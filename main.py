5cb1235f57b4e0b64a24222f60c4fa9e88977151
shubham shirke <shirkeshubham84@gmail.com>
2023-06-12 17:32:52 +0530

    [Shubham] Add: provides a remove contact option to user.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 @Author: shubham shirke
 @Date: 2023-06-12 10:30:30
 @Last Modified by: shubham shirke
-@Last Modified time: 2022-06-12 4:30:30
-@Title : To update existing contact in address book.
+@Last Modified time: 2022-06-12 17:30:30
+@Title : To delete contact in address book.
 '''
 from AddressBook_Service import *
 
@@ -15,8 +15,9 @@ def main():
         print("-----------------------")
         print("1.Add contact")
         print("2.Update Contact")
-        print("3.Print Contact")
-        print("4.Exit")
+        print("3.Remove contact")
+        print("4.Print Contact")
+        print("5.Exit")
         print("-----------------------")
         choice = int(input("Enter choice: "))  
         if choice == 1:
@@ -27,6 +28,10 @@ def main():
             address_book.update_contact(first_name)
 
         elif choice == 3:
+            first_name = input("Enter first name to remove: ")
+            address_book.remove_contact(first_name)
+
+        elif choice == 4:
             address_book.display_contact()
 
         elif choice == 3:
