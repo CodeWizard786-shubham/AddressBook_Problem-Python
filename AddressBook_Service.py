a79e89c6c5917ba36c78bb385445ff96616b411d
shubham shirke <shirkeshubham84@gmail.com>
2023-06-12 17:05:30 +0530

    [Shubham] Add: update function to the update existing contact.

--- AddressBook_Service.py
+++ AddressBook_Service.py
@@ -9,17 +9,18 @@ class Contact:
         self.zip = zip
         self.phone_number = phone_number
         self.email = email
-
-
+    
     def __str__(self):
         return (f" First name: {self.first_name}\n Last Name: {self.last_name}\n Address: {self.address}\n Name: {self.city}\n Zip: {self.zip}\n Phone-Number: {self.phone_number}\n Email: {self.email}\n")
 
 
 # Addressbook Operations
 class AddressBook:
+    
     def __init__(self):
         self.contacts = []
 
+    # add contacts
     def add_contact(self):
         """
         Description : 
@@ -42,7 +43,43 @@ class AddressBook:
         self.contacts.append(contact)
         print("Contact Added Succesfully")
 
-        
+
+    def fetch_contact(self,first_name):
+        for contact in self.contacts:
+            if contact.first_name == first_name:
+                return contact
+        return None
+    
+    # update address Book        
+    def update_contact(self,first_name):
+        """
+        Description :   
+                    This function checks if the first name in present in contact 
+                    if it is present, take the inputs from user again ,find the index number and update the contact. 
+        Parameters :
+                    self : self.contacts list.
+        Returns :
+                    none
+                    prints update message.
+        """
+        contact = self.fetch_contact(first_name)
+        if contact:
+            first_name =input("Enter first name: ")
+            second_name = input("Enter second name: ")
+            address =input("Enter address: ")
+            city =input("Enter city: ")
+            state =input("Enter state: ")
+            zip =int(input("Enter zip: "))
+            phone_number =int(input("Enter phone number: "))
+            email_id = input("Enter email id: ")
+            new_contact = Contact(first_name,second_name,address,city,state,zip,phone_number,email_id)
+            index = self.contacts.index(contact)
+            self.contacts[index] = new_contact
+            print("Contact updated successfully")
+        else:
+            input("Contact not found. Hit enter to continue")
+
+    # display address Book
     def display_contact(self):
         """
         Description : 
@@ -59,6 +96,7 @@ class AddressBook:
                 print(contact)
             else:
                 print("Contact not found")
+        input("Contacts displayed, Hit Enter to continue!")
 
 
 
