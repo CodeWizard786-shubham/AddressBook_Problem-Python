669345b5412d8ada363645fe857e9fc791aee75a
shubham shirke <shirkeshubham84@gmail.com>
2023-06-12 19:55:31 +0530

    [Shubham] Add: Refactor code to perform operations in dictionary.

--- AddressBook_Service.py
+++ AddressBook_Service.py
@@ -11,14 +11,14 @@ class Contact:
         self.email = email
     
     def __str__(self):
-        return (f" First name: {self.first_name}\n Last Name: {self.last_name}\n Address: {self.address}\n Name: {self.city}\n Zip: {self.zip}\n Phone-Number: {self.phone_number}\n Email: {self.email}\n")
+        return (f" First name: {self.first_name}, Last Name: {self.last_name}, Address: {self.address}, City: {self.city}, Zip: {self.zip}, Phone-Number: {self.phone_number}, Email: {self.email} ")
 
 
 # Addressbook Operations
 class AddressBook:
     
     def __init__(self):
-        self.contacts = []
+        self.contacts = {}
 
     # add contacts
     def add_contact(self):
@@ -31,6 +31,7 @@ class AddressBook:
                 none
                 prints contact added successfully
         """
+        print("Please enter contact details to add contact")
         first_name =input("Enter first name: ")
         second_name = input("Enter second name: ")
         address =input("Enter address: ")
@@ -40,22 +41,22 @@ class AddressBook:
         phone_number =int(input("Enter phone number: "))
         email_id = input("Enter email id: ")
         contact = Contact(first_name,second_name,address,city,state,zip,phone_number,email_id)
-        self.contacts.append(contact)
+        self.contacts[first_name]= contact
         print("Contact Added Succesfully")
 
 
-    def fetch_contact(self,first_name):
+    def fetch_contact(self, first_name):
         """
-        Description : 
-                This function fetches contact from contact list uing first name.
-        Parameters :
-                first_name : first name of the contact to remove.
-        Returns :
-                contact : contact from contacts list.
+        Description:
+                This function fetches a contact from the contacts list using the first name.
+        Parameters:
+                first_name: First name of the contact to retrieve.
+        Returns:
+                contact: The contact from the contacts list.
         """
-        for contact in self.contacts:
-            if contact.first_name == first_name:
-                return contact
+        for contact_key, contact_value in self.contacts.items():
+            if contact_key == first_name:
+                return contact_value
         return None
     
     # update address Book        
@@ -63,7 +64,7 @@ class AddressBook:
         """
         Description :   
                     This function checks if the first name in present in contact 
-                    if it is present, take the inputs from user again ,find the index number and update the contact. 
+                    if it is present, take the inputs from user again ,update the contact as per key from contacts dictionary. 
         Parameters :
                     self : self.contacts list.
         Returns :
@@ -72,6 +73,7 @@ class AddressBook:
         """
         contact = self.fetch_contact(first_name)
         if contact:
+            print("Please enter contact details for update")
             first_name =input("Enter first name: ")
             second_name = input("Enter second name: ")
             address =input("Enter address: ")
@@ -81,8 +83,7 @@ class AddressBook:
             phone_number =int(input("Enter phone number: "))
             email_id = input("Enter email id: ")
             new_contact = Contact(first_name,second_name,address,city,state,zip,phone_number,email_id)
-            index = self.contacts.index(contact)
-            self.contacts[index] = new_contact
+            self.contacts[first_name] = new_contact
             print("Contact updated successfully")
         else:
             input("Contact not found. Hit enter to continue")
@@ -102,7 +103,7 @@ class AddressBook:
         """
         contact = self.fetch_contact(first_name)
         if contact:
-            self.contacts.remove(contact)
+            del self.contacts[first_name]
             print("Contact removed successfully")
         else:
             input("Contact not found. Hit enter to continue")
@@ -114,17 +115,18 @@ class AddressBook:
         Description : 
                 This function prints the object of class contact.
         Parameters :
-                contact : return object from add_contact function.
+                self : contacts dict containing multiple contacts 
         Returns:
                 none
-                prints contact object
+                prints key value from contact object
         """
-        # iterate over contacts list to print each contact
-        for contact in self.contacts:
-            if contact in self.contacts:
-                print(contact)
-            else:
-                print("Contact not found")
+        # iterate over contacts dict to print each contact
+        print("|-------------------------------------------------------------------------------------------------|")
+        for key ,values in self.contacts.items():
+                print(f"{key} \n [{values}]")
+                print()
+        print("|-------------------------------------------------------------------------------------------------|")
+
         input("Contacts displayed, Hit Enter to continue!")
 
 
