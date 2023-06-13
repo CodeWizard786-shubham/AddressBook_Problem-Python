7c9792ad162b03706719fcc833e6382b30f1593e
shubham shirke <shirkeshubham84@gmail.com>
2023-06-13 15:07:32 +0530

    [Shubham] Add: Manage multiple address book interface

--- main.py
+++ main.py
@@ -2,45 +2,51 @@
 @Author: shubham shirke
 @Date: 2023-06-12 10:30:30
 @Last Modified by: shubham shirke
-@Last Modified time: 2022-06-12 17:42:30
-@Title : To add multiple contacts in dictionary in address book.
+@Last Modified time: 2022-06-13 12:30:30
+@Title : To add multiple address books in dictionary in address book system.
 '''
 from AddressBook_Service import *
 
 def main():
+    """
+    Description : 
+            This function is where the code execution starts. It provides user a interface to work with address book system.
+    Parameters:
+            none
+    Returns : 
+            none
+            displays menu driven interface.
+    """
     address_book = AddressBook()  # creating instance of a class
+
     while True:
         print()
-        print("--Address Book System--")
-        print("-----------------------")
-        print("1.Add contact")
-        print("2.Update Contact")
-        print("3.Remove contact")
-        print("4.Print Contact")
+        print("--AddressBook System--")
+        print("----------------------")
+        print("1.Add AddressBook")
+        print("2.Remove AddressBook")
+        print("3.Edit AddressBook")
+        print("4.Print AddressBook")
         print("5.Exit")
-        print("-----------------------")
-        choice = int(input("Enter choice: "))  
-        if choice == 1:
-            address_book.add_contact()
-
-        elif choice == 2:
-            first_name = input("Enter first name to update: ")
-            address_book.update_contact(first_name)
-
-        elif choice == 3:
-            first_name = input("Enter first name to remove: ")
-            address_book.remove_contact(first_name)
-
-        elif choice == 4:
-            address_book.display_contact()
-
-        elif choice == 5:
+        print("-------------------")
+        user_choice = int(input("Enter choice: "))
+        if user_choice == 1:
+            address_book_name = input("Enter Address Book name: ")
+            address_book.add_address_books(address_book_name)
+        elif user_choice == 2:
+            address_book_name = input("Enter Address Book name to remove: ")
+            address_book.remove_address_book(address_book_name)
+        elif user_choice == 3:
+            address_book_name = input("Enter Address book name to update: ")
+            address_book.update_address_book(address_book_name)
+        elif user_choice == 4:
+            address_book.display_address_books()
+        elif user_choice == 4:
+            print("Thank you for using Address Book system ")
             return False
-        else:
-            print("[error] Selected option is wrong")
-            return main()
-            
-            
+        else :
+            print("[error] : enter valid option")
+
 
 if __name__ == "__main__":
     main()
